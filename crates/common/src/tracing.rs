0bf637276721def7b7472e849d376adec8455d68
0xKitsune <0xKitsune@protonmail.com>
2023-11-07 10:39:04 -0500

    updated `get_log_directory`

--- crates/common/src/tracing.rs
+++ crates/common/src/tracing.rs
@@ -87,16 +87,27 @@ pub fn trace_to_headers(headers: &mut http::HeaderMap) {
     });
 }
 
-//TODO: clean this up and propagate errors
-pub fn get_log_directory() -> PathBuf {
-    let home_dir = dirs::home_dir().expect("Could not find home directory");
+/// Platform agnostic function to get the path to the log directory. If the directory does not
+/// exist, it will be created.
+///
+/// # Returns
+/// * `Ok(PathBuf)` containing the path to the `.logs` directory in the user's home directory.
+/// * `Err(io::Error)` if the home directory cannot be determined, or the `.logs` directory
+///   cannot be created.
+///
+/// # Errors
+/// This function will return an `Err` if the home directory cannot be found or the `.logs`
+/// directory cannot be created. It does not guarantee that the `.logs` directory is writable.
+pub fn get_log_directory() -> Result<PathBuf, io::Error> {
+    let home_dir = dirs::home_dir().ok_or(io::ErrorKind::NotFound)?;
     let log_dir = home_dir.join(".logs");
-    // Create the .logs directory if it does not exist
+
+    // Create the `.logs` directory if it does not exist
     if !log_dir.exists() {
-        fs::create_dir_all(&log_dir).expect("Could not create .logs directory");
+        fs::create_dir_all(&log_dir)?;
     }
 
-    log_dir
+    Ok(log_dir)
 }
 
 pub struct DataDogFormat;
