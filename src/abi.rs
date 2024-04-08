d011688edd85f0913903f9041196c09183c1417b
0xKitsune <0xKitsune@protonmail.com>
2024-04-08 09:25:29 -0400

    remove staging function sig

--- src/abi.rs
+++ src/abi.rs
@@ -7,7 +7,6 @@ abigen!(
         event TreeChanged(uint256 indexed preRoot, uint8 indexed kind, uint256 indexed postRoot)
         function registerIdentities(uint256[8] calldata insertionProof, uint256 preRoot, uint32 startIndex, uint256[] calldata identityCommitments, uint256 postRoot) external
         function deleteIdentities(uint256[8] calldata deletionProof, bytes calldata packedDeletionIndices, uint256 preRoot, uint256 postRoot) external
-        function deleteIdentities(uint256[8] calldata deletionProof, uint32 batchSize, bytes calldata packedDeletionIndices, uint256 preRoot, uint256 postRoot) external
     ]"#;
 
     IBridgedWorldID,
