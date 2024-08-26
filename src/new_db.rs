95f7e4caa01b9c4abfec4dd23f3876cfbd3df82d
Dzejkop <jakubtrad@gmail.com>
2024-08-26 15:23:51 +0200

    Traitify

--- src/new_db.rs
+++ src/new_db.rs
@@ -1,30 +1,25 @@
+use axum::async_trait;
 use sqlx::{Acquire, Postgres};
 
-#[derive(Clone, Copy)]
-pub struct Db<T> {
-    inner: T,
-}
-
-impl<T> Db<T> {
-    pub fn new(inner: T) -> Self {
-        Db { inner }
-    }
-}
-
-impl<'c, T> Db<T>
-where
-    T: Acquire<'c, Database = Postgres> + Send,
+#[async_trait]
+pub trait Database<'c>:
+    Acquire<'c, Database = Postgres> + Send + Sync + Sized
 {
-    pub async fn fetch_user_id(self) -> sqlx::Result<Option<i32>> {
-        let mut conn = self.inner.acquire().await?;
-        let executor = conn.as_mut();
+    async fn fetch_user_id(self) -> sqlx::Result<Option<i32>> {
+        let mut conn = self.acquire().await?;
 
         let x: Option<(i32,)> = sqlx::query_as("SELECT id FROM users")
-            .fetch_optional(executor)
+            .fetch_optional(&mut *conn)
             .await?;
 
         Ok(x.map(|(id,)| id))
     }
+}
+
+// Blanket implementation for all types that satisfy the trait bounds
+impl<'c, T> Database<'c> for T where
+    T: Acquire<'c, Database = Postgres> + Send + Sync
+{
 }
 
 #[cfg(test)]
@@ -40,13 +35,10 @@ mod tests {
                 .await
                 .unwrap();
 
-        let db = Db::new(&pool);
-
-        let id = db.fetch_user_id().await.unwrap();
+        let id = pool.fetch_user_id().await.unwrap();
         assert_eq!(id, None);
 
-        // Can reuse because db &Pool is Copy and fetch_user_id borrows self
-        let id = db.fetch_user_id().await.unwrap();
+        let id = pool.fetch_user_id().await.unwrap();
         assert_eq!(id, None);
     }
 
@@ -58,14 +50,11 @@ mod tests {
         .await
         .unwrap();
 
-        let db = Db::new(&mut conn);
-
-        let id = db.fetch_user_id().await.unwrap();
+        let id = conn.fetch_user_id().await.unwrap();
         assert_eq!(id, None);
 
-        // Doesn't work because &mut T is neither Clone nor Copy
-        // let id = db.fetch_user_id().await.unwrap();
-        // assert_eq!(id, None);
+        let id = conn.fetch_user_id().await.unwrap();
+        assert_eq!(id, None);
     }
 
     #[tokio::test]
@@ -76,17 +65,12 @@ mod tests {
                 .unwrap();
         let mut tx = pool.begin().await.unwrap();
 
-        let db = Db::new(&mut tx);
-
-        let id = db.fetch_user_id().await.unwrap();
+        let id = tx.fetch_user_id().await.unwrap();
         assert_eq!(id, None);
 
-        // Doesn't work because &mut T is neither Clone nor Copy
-        // let id = db.fetch_user_id().await.unwrap();
-        // assert_eq!(id, None);
-
-        // This works though
-        let id = Db::new(&mut tx).fetch_user_id().await.unwrap();
+        let id = tx.fetch_user_id().await.unwrap();
         assert_eq!(id, None);
+
+        tx.commit().await.unwrap();
     }
 }
