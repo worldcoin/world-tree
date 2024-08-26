07e21376190311899608cfc3f33b40754184f897
Dzejkop <jakubtrad@gmail.com>
2024-08-26 15:34:03 +0200

    Add Db creation/migration methods

--- src/new_db.rs
+++ src/new_db.rs
@@ -1,8 +1,40 @@
-use axum::async_trait;
-use sqlx::{Acquire, Postgres};
+use std::ops::{Deref, DerefMut};
+
+use async_trait::async_trait;
+use sqlx::{Acquire, Error, PgPool, Postgres};
+
+pub struct Db {
+    pool: PgPool,
+}
+
+impl Db {
+    pub async fn new(database_url: &str) -> Result<Self, Error> {
+        let pool = PgPool::connect(database_url).await?;
+        Ok(Self { pool })
+    }
+
+    pub async fn migrate(&self) -> Result<(), Error> {
+        sqlx::migrate!("./migrations").run(&self.pool).await?;
+        Ok(())
+    }
+}
+
+impl Deref for Db {
+    type Target = PgPool;
+
+    fn deref(&self) -> &Self::Target {
+        &self.pool
+    }
+}
+
+impl DerefMut for Db {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.pool
+    }
+}
 
 #[async_trait]
-pub trait Database<'c>:
+pub trait DbMethods<'c>:
     Acquire<'c, Database = Postgres> + Send + Sync + Sized
 {
     async fn fetch_user_id(self) -> sqlx::Result<Option<i32>> {
@@ -17,16 +49,30 @@ pub trait Database<'c>:
 }
 
 // Blanket implementation for all types that satisfy the trait bounds
-impl<'c, T> Database<'c> for T where
-    T: Acquire<'c, Database = Postgres> + Send + Sync
+impl<'c, T> DbMethods<'c> for T where
+    T: Acquire<'c, Database = Postgres> + Send + Sync + Sized
 {
 }
 
 #[cfg(test)]
 mod tests {
-    use sqlx::{Connection, PgConnection, PgPool};
+    use sqlx::{Connection, PgConnection};
 
     use super::*;
+
+    #[tokio::test]
+    async fn works_for_db() {
+        let db = Db::new("postgres://postgres:password@localhost/testdb")
+            .await
+            .unwrap();
+        db.migrate().await.unwrap();
+
+        let id = db.fetch_user_id().await.unwrap();
+        assert_eq!(id, None);
+
+        let id = db.fetch_user_id().await.unwrap();
+        assert_eq!(id, None);
+    }
 
     #[tokio::test]
     async fn works_for_pool() {
@@ -50,25 +96,24 @@ mod tests {
         .await
         .unwrap();
 
-        let id = conn.fetch_user_id().await.unwrap();
+        let id = (&mut conn).fetch_user_id().await.unwrap();
         assert_eq!(id, None);
 
-        let id = conn.fetch_user_id().await.unwrap();
+        let id = (&mut conn).fetch_user_id().await.unwrap();
         assert_eq!(id, None);
     }
 
     #[tokio::test]
     async fn works_for_transaction() {
-        let pool =
-            PgPool::connect("postgres://postgres:password@localhost/testdb")
-                .await
-                .unwrap();
-        let mut tx = pool.begin().await.unwrap();
+        let db = Db::new("postgres://postgres:password@localhost/testdb")
+            .await
+            .unwrap();
+        let mut tx = db.begin().await.unwrap();
 
-        let id = tx.fetch_user_id().await.unwrap();
+        let id = (&mut tx).fetch_user_id().await.unwrap();
         assert_eq!(id, None);
 
-        let id = tx.fetch_user_id().await.unwrap();
+        let id = (&mut tx).fetch_user_id().await.unwrap();
         assert_eq!(id, None);
 
         tx.commit().await.unwrap();
