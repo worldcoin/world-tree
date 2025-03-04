c8a19708979602155d5a2e602c2c7d17d3751db6
Dzejkop <jakubtrad@gmail.com>
2025-03-04 16:43:38 +0100

    fix

--- tests/fixtures/integration_contracts/run.sh
+++ tests/fixtures/integration_contracts/run.sh
@@ -50,7 +50,7 @@ ANVIL_PID=$!
 PRIV_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
 # Wait until Anvil starts in the background
-while ! nc -z "$HOST" "$PORT"; do
+while ! bash -c "echo > /dev/tcp/${HOST}/${PORT}" 2>/dev/null; do
   sleep 1
 done
 
