5718510b783398c6a678639cdce2206c0003777c
Dzejkop <jakubtrad@gmail.com>
2025-03-04 16:26:00 +0100

    smarter wait

--- tests/fixtures/integration_contracts/run.sh
+++ tests/fixtures/integration_contracts/run.sh
@@ -50,7 +50,9 @@ ANVIL_PID=$!
 PRIV_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
 # Wait until Anvil starts in the background
-sleep 5
+while ! nc -z "$HOST" "$PORT"; do
+  sleep 1
+done
 
 # Create contract
 forge create --broadcast --private-key $PRIV_KEY ${CONTRACT}
