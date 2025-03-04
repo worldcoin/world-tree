c9ffcc5af870327f255606a2683aefbc6c33df70
Dzejkop <jakubtrad@gmail.com>
2025-03-04 16:24:38 +0100

    longer sleep?

--- tests/fixtures/integration_contracts/run.sh
+++ tests/fixtures/integration_contracts/run.sh
@@ -50,7 +50,7 @@ ANVIL_PID=$!
 PRIV_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
 # Wait until Anvil starts in the background
-sleep 1
+sleep 5
 
 # Create contract
 forge create --broadcast --private-key $PRIV_KEY ${CONTRACT}
